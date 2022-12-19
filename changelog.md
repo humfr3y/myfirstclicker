3a05d8651e7637f6a548242b823bd627627464d6
Humfrey <madkotodax@gmail.com>
2022-12-20 02:05:57 +0300

    summary (required)

---
+++ changelog.md
@@ -0,0 +1,23 @@
+20.12.2022 | My 1st clicker 0.2. SAVE Update (Codename: Save):
+- You can finally save n load the game by bottom buttons (but text for upgrades doesn't refresh)
+- Changelog button added
+- Now CSS lines have moved to a separate .css file
+- BETA button changes 
+[Dev comment]
+Actually i did it in one day but now i have 20th December. Most funny thing that i spent for saving not much time. Next goal is new interesting upgrades.
+
+19.12.2022 | My 1st clicker 0.1. Graphic Update (Codename: BUTTON):
+- Game has beautiful coin icon instead of depressed button
+- Upgrades buttons became a little better
+- A little decreasing of cost formula for upgrades
+[Dev comment]
+Well i continued it and i did something good even with zero html and css skills, it's really impressive and i really should learn them both later.
+There a really much things which i should do, and which i dont know how to do. Also i tried to add icons for buttons but when you buying upgrades it disappears, i should to link it somehow...
+
+18.12.2022 | My 1st Clicker 0.0. Null Update (Codename: NULL):
+- NEW MECHANIC! You need to clicking on the button for gaining coins
+- NEW 2 UPGRADES! First increases coin per click by 1, second increases bonus of first one by 10%
+- Some bugs like you sometimes cant get bonus from 2nd upgrade. Idk why.
+[Dev comment]
+This is a first game made by me from scratch, and i spent for it a some time. Well it's actually my second game, but that first made it's just mod.
+Idk if i want to continue it.
--- javascript/clicker.js
+++ javascript/clicker.js
@@ -29,7 +29,7 @@ function doubleboost(ele2) {
     else {ele2.innerHTML = "Upgrade gives +" + db + " coins per click. You need for next upgrade " + (doubleprice+10).toFixed(0) + " coins."}
     if (data) {document.getElementById('ele').innerHTML = money.toFixed(0) + " монет. Вы купили первое улучшение.";}
     else {document.getElementById('ele').innerHTML = money.toFixed(0) + " coins. You have bought first upgrade.";}}
-    return doubleprice;
+    return amount, db;
 };
 function multiboost(ele3) {
     if (mamount==0) {mboost=1} 
@@ -42,5 +42,46 @@ function multiboost(ele3) {
     else {document.getElementById('ele').innerHTML = money.toFixed(0) + " coins. You have bought second upgrade.";}};
     if (mamount>=1) {mboost=1+(mb/10)};
     if (mamount>=1) {multiprice=(mamount+1)*94.9} else {multiprice=100};
-} 
-
+    return mamount, mb, mboost;
+}
+function savezone (e) {
+if (localStorage)
+    {
+        if (confirm("Хотите ли сохранить игру? / Do you want save the game?"))
+        {
+            localStorage["money_mfs_count"] = money;
+            localStorage["1st_mfs_count"] = amount;
+            localStorage["2nd_mfs_count"] = mamount;
+            localStorage["db_mfs_count"] = db;
+            localStorage["mb_mfs_count"] = mb;
+            localStorage["total_mfs_count"] = total;
+            localStorage["mboost_mfs_count"] = mboost;
+        }
+    }
+}
+function loadzone () {
+    if (localStorage)   {
+        if (confirm("Хотите ли вы загрузить игру? / Do you want load the game?"))
+        {
+        var savedmoney = localStorage["money_mfs_count"];
+        var saved1st = localStorage["1st_mfs_count"];
+        var saved2nd = localStorage["2nd_mfs_count"];
+        var saveddb = localStorage["db_mfs_count"];
+        var savedmb = localStorage["mb_mfs_count"];
+        var savedtotal = localStorage["total_mfs_count"];
+        var savedmboost = localStorage["mboost_mfs_count"];
+        if (savedmoney != null) money=Number(savedmoney);
+        if (saved1st != null) amount=Number(saved1st);
+        if (saved2nd != null) mamount=Number(saved2nd);
+        if (saveddb != null) db=Number(saveddb);
+        if (savedmb != null) mb=Number(savedmb);
+        if (savedtotal != null) total=Number(savedtotal);
+        if (savedmboost != null) mboost=Number(savedmboost);
+        if (data) {document.getElementById('ele').innerHTML = money.toFixed(0) + " монет. Вы получаете за нажатие " + ((db*mboost)+1).toFixed(2)+ " монет";}
+        else {document.getElementById('ele').innerHTML = money.toFixed(0) + " coins. You've earning for a click " + ((db*mboost)+1).toFixed(2) + " coins";};
+        }
+    }
+}
+function changelog(){
+    
+}
